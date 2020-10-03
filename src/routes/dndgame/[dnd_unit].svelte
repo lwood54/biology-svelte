b1fa68ce1d272666a195bcef8697ba41502548b9
Logan Wood <lwood3499@gmail.com>
2020-10-03 12:17:02 -0500

    fixed round to round transition fade

--- src/routes/dndgame/[dnd_unit].svelte
+++ src/routes/dndgame/[dnd_unit].svelte
@@ -22,11 +22,11 @@
 </script>
 
 {#if updatedRound === 1}
-	<div out:fade>
+	<div out:fade={{ duration: 500 }}>
 		<GameBoard colHeadings={dnd_content.dndgame1} pieces={dnd_content.pieces1} on:checkround={updateRound} round={1} />
 	</div>
 {:else}
-	<div in:fade>
+	<div in:fade={{ delay: 600, duration: 500 }}>
 		<GameBoard colHeadings={dnd_content.dndgame2} pieces={dnd_content.pieces2} on:checkround={updateRound} round={2} />
 	</div>
 {/if}
