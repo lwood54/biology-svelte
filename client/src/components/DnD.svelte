1195796d7411a5f8928cb76b1e247a4c9d237f2f
Logan Wood <lwood3499@gmail.com>
2020-10-05 23:01:12 -0500

    changes

--- client/src/components/DnD.svelte
+++ client/src/components/DnD.svelte
@@ -11,16 +11,6 @@
 	$: pieces = updatedRound === 1 ? dnd_content.pieces1 : dnd_content.pieces2;
 	$: round = updatedRound;
 
-	const store = {
-		totalCorrect,
-		totalWrong,
-		round1Correct,
-		round1Wrong,
-		round2Correct,
-		round2Wrong,
-		firstLoad,
-	};
-
 	const updateRound = (e) => {
 		updatedRound = e.detail;
 	};
@@ -28,51 +18,38 @@
 
 {#if updatedRound === 1}
 	<div in:fade={{ duration: 300 }} out:fade={{ duration: 500 }}>
-		<GameBoard {store} title={dnd_content.title} {colHeadings} {pieces} on:checkround={updateRound} {round} />
+		<GameBoard
+			{totalCorrect}
+			{totalWrong}
+			{round1Correct}
+			{round1Wrong}
+			{round2Correct}
+			{round2Wrong}
+			{firstLoad}
+			title={dnd_content.title}
+			{colHeadings}
+			{pieces}
+			on:checkround={updateRound}
+			{round} />
 	</div>
 {:else if updatedRound === 2}
 	<div in:fade={{ delay: 600, duration: 500 }} out:fly={{ x: 200, duration: 500 }}>
-		<GameBoard {store} title={dnd_content.title} {colHeadings} {pieces} on:checkround={updateRound} {round} />
+		<GameBoard
+			{totalCorrect}
+			{totalWrong}
+			{round1Correct}
+			{round1Wrong}
+			{round2Correct}
+			{round2Wrong}
+			{firstLoad}
+			title={dnd_content.title}
+			{colHeadings}
+			{pieces}
+			on:checkround={updateRound}
+			{round} />
 	</div>
 {:else if updatedRound === 3}
 	<div in:fly={{ y: 200, delay: 600 }}>
 		<GameOver totalCorrect={$totalCorrect} totalWrong={$totalWrong} />
 	</div>
 {/if}
-<!-- {#if updatedRound === 1}
-	<div in:fade={{ duration: 300 }} out:fade={{ duration: 500 }}>
-		<GameBoard
-			{totalCorrect}
-			{totalWrong}
-			{round1Correct}
-			{round1Wrong}
-			{round2Correct}
-			{round2Wrong}
-			{firstLoad}
-			title={dnd_content.title}
-			{colHeadings}
-			{pieces}
-			on:checkround={updateRound}
-			{round} />
-	</div>
-{:else if updatedRound === 2}
-	<div in:fade={{ delay: 600, duration: 500 }} out:fly={{ x: 200, duration: 500 }}>
-		<GameBoard
-			{totalCorrect}
-			{totalWrong}
-			{round1Correct}
-			{round1Wrong}
-			{round2Correct}
-			{round2Wrong}
-			{firstLoad}
-			title={dnd_content.title}
-			{colHeadings}
-			{pieces}
-			on:checkround={updateRound}
-			{round} />
-	</div>
-{:else if updatedRound === 3}
-	<div in:fly={{ y: 200, delay: 600 }}>
-		<GameOver totalCorrect={$totalCorrect} totalWrong={$totalWrong} />
-	</div>
-{/if} -->
