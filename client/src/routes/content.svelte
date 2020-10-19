09f9802e238869db41623f1c8b1da4f02bbe8d7b
Logan Wood <lwood3499@gmail.com>
2020-10-18 19:19:31 -0500

    game links page update and unit 3 game content added

--- client/src/routes/content.svelte
+++ client/src/routes/content.svelte
@@ -1,7 +1,10 @@
-<script>
+<script lang="ts">
+  import { fade } from "svelte/transition";
 </script>
+
+<div in:fade={{ delay: 500, duration: 500 }}>
+  <h1>Content</h1>
+</div>
 
 <style>
 </style>
-
-<h1>Content</h1>
